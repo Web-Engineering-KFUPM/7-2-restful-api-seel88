f303c76b9b5a79191244448760d5949abc1f6552
seel88 <s202270080@kfupm.edu.sa>
2026-04-15 08:28:52 +0300

    TASK 2: Complete Song schema and model

--- 7-2-restful-api/server/models/song.model.js
+++ 7-2-restful-api/server/models/song.model.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
 // db schema
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String, 
+    required: true
+  },
+  artist: {
+    type: String, 
+    required: true
+  },
+  year: {
+    type: Number, 
+    required: true
+  }
+});
 
 export const Song = mongoose.model("Song", songSchema);
